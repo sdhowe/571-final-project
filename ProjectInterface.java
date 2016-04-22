dd68ca728a963e76257eaadb0d7c9b4882be336f
aldaang <alis.dang@gmail.com>
2016-04-22 14:08:03 -0600

    Redirected KMeans output to text area on GUI

--- ProjectInterface.java
+++ ProjectInterface.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.PrintStream;
 
 /**
  * Created by Alexander Dang on 4/3/2016.
@@ -11,13 +12,21 @@ public class ProjectInterface extends JFrame {
     private JSpinner spinner1;
     private JComboBox comboBox2;
     private JPanel rootPanel;
+    private JTextArea textArea1;
+    private JScrollPane scrollPane;
+    private PrintStream standardOut;
 
     public ProjectInterface() {
         setContentPane(rootPanel);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
         setVisible(true);
+
+        PrintStream printStream = new PrintStream(new CustomOutputStream(textArea1));
+        System.setOut(printStream);
 
         simulateButton.addActionListener(new ActionListener() {
             @Override
@@ -28,15 +37,11 @@ public class ProjectInterface extends JFrame {
 
                 int algorithmIndex = comboBox1.getSelectedIndex();
 
-                String combo = obj.toString();
-
-                Object obj2 = comboBox2.getSelectedItem();
-                String combo2 = obj2.toString();
-                System.out.println("Selected data set: " + combo2);
+                //String combo = obj.toString();
 
                 switch (algorithmIndex) {
                     case 0:
-                        System.out.println("Selected algorithm: " + combo);
+                        //System.out.println("Selected algorithm: " + combo);
                         long startTime = System.nanoTime();
                         KMeans kmeans = new KMeans(kValue);
                         kmeans.init(kValue);
@@ -46,13 +51,13 @@ public class ProjectInterface extends JFrame {
                         System.out.println("Time to run: " + duration + "ms");
                         break;
                     case 1:
-                        System.out.println("Selected algorithm: " + combo);
+                        //System.out.println("Selected algorithm: " + combo);
                         break;
                     case 2:
-                        System.out.println("Selected algorithm: " + combo);
+                        //System.out.println("Selected algorithm: " + combo);
                         break;
                     case 3:
-                        System.out.println("Selected algorithm: " + combo);
+                        //System.out.println("Selected algorithm: " + combo);
                         break;
                 }
             }
